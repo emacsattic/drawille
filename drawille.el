20e5713cdff6835d5fb86bde6141d1b2288c00e9
Josuah Demangeon <josuah.demangeon>
2015-12-13 17:08:09 +0100

    Added a function to draw paths Including possibilities for circles or polygons as well. #1

--- drawille.el
+++ drawille.el
@@ -76,10 +76,10 @@
 (defun drawille-vector-to-char (vector)
   "Translate a VECTOR to a corresponding braille character."
   (cl-loop for dot across vector
-	   for offset across drawille-braille-table collect
-	   (* dot offset)
-	   into offsets finally return
-	   (apply '+ drawille-braille-unicode-offset offsets)))
+           for offset across drawille-braille-table collect
+           (* dot offset)
+           into offsets finally return
+           (apply '+ drawille-braille-unicode-offset offsets)))
 
 (drawille-vector-to-char [0 1 1 1 0 0 0 0])
 
@@ -89,23 +89,23 @@
 (defun drawille-char-to-vector (char)
   "Translate a braille CHAR to a corresponding vector."
   (cl-loop with char-offset = (- char drawille-braille-unicode-offset)
-	   with result = (make-vector 8 nil)
-	   for dot-index across drawille-braille-reverse-table
-	   for dot-offset = (aref drawille-braille-table dot-index)
-	   do
-	   (aset result dot-index
-		 (if (< char-offset dot-offset) 0
-		   (setq char-offset (- char-offset dot-offset)) 1))
-	   finally return result))
+           with result = (make-vector 8 nil)
+           for dot-index across drawille-braille-reverse-table
+           for dot-offset = (aref drawille-braille-table dot-index)
+           do
+           (aset result dot-index
+                 (if (< char-offset dot-offset) 0
+                   (setq char-offset (- char-offset dot-offset)) 1))
+           finally return result))
 
 (drawille-char-to-vector (drawille-vector-to-char [0 1 0 1 1 1 0 0]))
 
 (defun drawille-vector-at-pos (matrix row column)
   "Return a braille char corresponding to MATRIX at ROW, COLUMN."
   (cl-loop for i from 0 to 3 vconcat
-	   (cl-loop for j from 0 to 1 vconcat
-		    (vector (aref (aref matrix (+ row i))
-				  (+ column j))))))
+           (cl-loop for j from 0 to 1 vconcat
+                    (vector (aref (aref matrix (+ row i))
+                                  (+ column j))))))
 
 (defun drawille-fill-matrix (matrix)
   "Return a MATRIX filled until there are a multiple of 4 of rows."
@@ -114,44 +114,44 @@
     (if (= (% height 4) 0)
         matrix
       (vconcat (make-vector (- 4 (% height 4)) (make-vector width 0))
-	       matrix ))))
+               matrix ))))
 
-(defun drawille-matrix (unfilled-matrix)
+(defun drawille-from-matrix (unfilled-matrix)
   "Fill an UNFILLED-MATRIX and subdivides it into a matrix of vector.
 It will then call `drawille-vector-to-char' to fill rows, then
 columns."
   (cl-loop with matrix = (drawille-fill-matrix unfilled-matrix)
-	   with width = (length (aref matrix 0))
-	   with height = (length matrix)
-	   for i from 0 to (1- (floor height 4))
-	   concat
-	   (cl-loop for j from 0 to (1- (floor width 2)) concat
-		    (char-to-string
-		     (drawille-vector-to-char
-		      (drawille-vector-at-pos matrix (* 4 i) (* 2 j))))
-		    into line finally return (concat line "\n"))))
+           with width = (length (aref matrix 0))
+           with height = (length matrix)
+           for i from 0 to (1- (floor height 4))
+           concat
+           (cl-loop for j from 0 to (1- (floor width 2)) concat
+                    (char-to-string
+                     (drawille-vector-to-char
+                      (drawille-vector-at-pos matrix (* 4 i) (* 2 j))))
+                    into line finally return (concat line "\n"))))
 
 (defun drawille-grid (drawille row column)
   "Format a DRAWILLE string into a vector of strings (the grid).
-Its size may be adjusted if so that a dot at (ROW, COLUMN) can be
-displayed."
+If needed, its size is adjusted so that a dot at (ROW, COLUMN)
+can be displayed."
   (let* ((grid (apply 'vector (split-string drawille "\n" t)))
-	 (width (length (aref grid 0)))
-	 (height (length grid)))
+         (width (length (aref grid 0)))
+         (height (length grid)))
     (when (> (1+ column) (* 2 width))
       (setq grid
-	    (cl-loop
-	     for grid-row across grid vconcat
-	     (vector (concat grid-row (make-string
-				       (1+ (- (floor column 2) width))
-				       #x2800))))))
+            (cl-loop
+             for grid-row across grid vconcat
+             (vector (concat grid-row (make-string
+                                       (1+ (- (floor column 2) width))
+                                       #x2800))))))
     (when (> (1+ row) (* 4 height))
       (setq grid
-	    (vconcat
-	     (cl-loop
-	      for i from 0 to (- (floor row 4) height) vconcat
-	      (vector (make-string (length (aref grid 0)) #x2800)))
-	     grid)))
+            (vconcat
+             (cl-loop
+              for i from 0 to (- (floor row 4) height) vconcat
+              (vector (make-string (length (aref grid 0)) #x2800)))
+             grid)))
     grid))
 
 (defun drawille-draw-dot (drawille x y)
@@ -159,12 +159,14 @@ displayed."
 Coordinates starts at 0 at the bottom left, it can accept floats,
 but not negative cordinates.  Although, they can overflow at the
 rigt and at the top of the current matrix."
-  (let* ((n-x (round x)) (n-y (round y))
-	 (grid (drawille-grid drawille n-y n-x))
-	 (inverted-y (- (* 4 (length grid)) n-y 1)) ;Row to ordinate
-	 (grid-row (aref grid (floor inverted-y 4)))
-	 (vector (drawille-char-to-vector
-		  (aref grid-row (floor n-x 2)))))
+  (let* ((drawille (if (not drawille) (char-to-string #x2800)
+                   drawille))
+         (n-x (round x)) (n-y (round y))
+         (grid (drawille-grid drawille n-y n-x))
+         (inverted-y (- (* 4 (length grid)) n-y 1)) ;Row to ordinate
+         (grid-row (aref grid (floor inverted-y 4)))
+         (vector (drawille-char-to-vector
+                  (aref grid-row (floor n-x 2)))))
     (aset vector (+ (* 2 (% inverted-y 4)) (% n-x 2)) 1)
     (aset grid-row (floor n-x 2) (drawille-vector-to-char vector))
     (mapconcat 'concat grid "\n")))
@@ -182,38 +184,69 @@ rigt and at the top of the current matrix."
 
 (defun drawille-draw-line (drawille x1 y1 x2 y2)
   "On a DRAWILLE string, draw a line from X1, Y1 to X2, Y2."
-  (cl-loop with x-offset = (- x2 x1)
-	   with y-offset = (- y2 y1)
-	   for x in (if (= x-offset 0)
-			(make-list (round y-offset) 0)
-		      (number-sequence 0 x-offset
-				       (* (cl-signum x-offset)
-					  (min 1 (abs (/ (float x-offset)
-							 (float y-offset)))))))
-	   for y in (if (= y-offset 0)
-			(make-list (round x-offset) 0)
-		      (number-sequence 0 y-offset
-				       (* (cl-signum y-offset)
-					  (min 1 (abs (/ (float y-offset)
-							 (float x-offset)))))))
-	   do
-	   (setq drawille (drawille-draw-dot drawille (+ x1 x) (+ y1 y)))
-	   finally return drawille))
+  (cl-loop with drawille = (if (not drawille) (char-to-string #x2800)
+                             drawille)
+           with x-offset = (- x2 x1)
+           with y-offset = (- y2 y1)
+           for x in (if (= x-offset 0)
+                        (make-list (abs (round y-offset)) 0)
+                      (number-sequence 0 x-offset
+                                       (* (cl-signum x-offset)
+                                          (min 1 (abs (/ (float x-offset)
+                                                         (float y-offset)))))))
+           for y in (if (= y-offset 0)
+                        (make-list (abs (round x-offset)) 0)
+                      (number-sequence 0 y-offset
+                                       (* (cl-signum y-offset)
+                                          (min 1 (abs (/ (float y-offset)
+                                                         (float x-offset)))))))
+           do
+           (setq drawille (drawille-draw-dot drawille (+ x1 x) (+ y1 y)))
+           finally return drawille))
 
-(defun drawille-sparkline (&rest values)
+(defun drawille-draw-sparkline (drawille &rest values)
   "On a DRAWILLE string, add a plot representing each of the VALUES.
 The VALUES can be integers or float, positive or negative."
-  (cl-loop with min = (apply 'min values)
-	   with offset = (if (< min 0) (- min) 0)
-	   with drawille = (char-to-string #x2800)
-	   for x1 from 0
-	   for x2 from 1 to (length values)
-	   for y1 in values
-	   for y2 in (cdr values)
-	   do
-	   (setq drawille (drawille-draw-line drawille
-					      x1 (+ y1 offset)
-					      x2 (+ y2 offset)))
+  (cl-loop with drawille = (if (not drawille) (char-to-string #x2800)
+                             drawille)
+           with min = (apply 'min values)
+           with offset = (if (< min 0) (- min) 0)
+           for x1 from 0
+           for x2 from 1 to (length values)
+           for y1 in values
+           for y2 in (cdr values)
+           do
+           (setq drawille (drawille-draw-line drawille
+                                              x1 (+ y1 offset)
+                                              x2 (+ y2 offset)))
+           finally return drawille))
+
+(defun drawille-draw-path (drawille &rest coordinates)
+  "On a DRAWILLE draw a path for each COORDINATES.
+Coordinates are multiple lists in the form (X Y)."
+  (cl-loop with drawille = (if (not drawille) (char-to-string #x2800)
+                             drawille)
+	   with x-min = (cl-loop for coordinate in coordinates
+				 minimize (car coordinate))
+	   with y-min = (cl-loop for coordinate in coordinates
+				 minimize (cadr coordinate))
+	   with x-offset = (if (< x-min 0) (- x-min) 0)
+	   with y-offset = (if (< y-min 0) (- y-min) 0)
+           for coordinate1 in coordinates
+           for x1 = (car coordinate1)
+           for y1 = (cadr coordinate1)
+           for coordinate2 in (cdr coordinates)
+           for x2 = (car coordinate2)
+           for y2 = (cadr coordinate2)
+           do
+	   (message "(min: %s %s) (offset: %s %s) (1: %s %s) (2: %s %s)"
+		    x-min y-min x-offset y-offset
+		    (+ x-offset x1) (+ y-offset y1)
+		    (+ x-offset x2) (+ y-offset y2))
+	   (setq drawille (drawille-draw-line
+			   drawille
+			   (+ x-offset x1) (+ y-offset y1)
+			   (+ x-offset x2) (+ y-offset y2)))
 	   finally return drawille))
 
 ;; TODO Truncate the string if it overflow or automatically detect the
@@ -223,11 +256,11 @@ The VALUES can be integers or float, positive or negative."
   (cl-loop
    for string in string-list collect
    (substring (concat string (when (< (length string) column)
-			       (make-string (- column (length string))
-					    0)))
-	      0 column)))
+                               (make-string (- column (length string))
+                                            0)))
+              0 column)))
 
-(defun drawille-string (string column)
+(defun drawille-from-string (string column)
   "Transform a STRING to a minimap with COLUMN width.
 As vim-minimap does: https://github.com/severin-lemaignan/vim-minimap"
   (let* ((string-without-spaces
@@ -235,7 +268,7 @@ As vim-minimap does: https://github.com/severin-lemaignan/vim-minimap"
          (string-without-non-spaces
           (replace-regexp-in-string
            (concat "[^\n" (char-to-string 0) "\s]")
-	   (char-to-string 1) string-without-spaces))
+           (char-to-string 1) string-without-spaces))
          (string-list
           (split-string string-without-non-spaces "\n"))
          (filled-strings-vector
@@ -243,7 +276,7 @@ As vim-minimap does: https://github.com/severin-lemaignan/vim-minimap"
     (drawille-matrix (vconcat filled-strings-vector))))
 
 ;;;###autoload
-(defun drawille-buffer ()
+(defun drawille-from-buffer ()
   "Generate a drawille for current buffer."
   (interactive)
   (message "%s" (drawille-string (buffer-string) fill-column)))
